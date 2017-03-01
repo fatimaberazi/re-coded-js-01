a1e495abe3a7c1f1f531005f8f8bbab651528d88
Fatima Berazi <fatimaberaze1997@gmail.com>
2017-03-01 22:51:52 +0300

    Done

--- js/simple_data_binding.js
+++ js/simple_data_binding.js
@@ -1,3 +1,10 @@
 $(document).ready(function() {
-  
-});
+ typing(); 
+}); 
+function typing(){ 
+
+	$('#textArea').on('keydown',function () {
+		$('#myDiv').text("")//the same as null
+		$('#myDiv').text($('#textArea').val()); 
+	})
+}
