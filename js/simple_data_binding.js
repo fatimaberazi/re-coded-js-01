e80c6b8d1beee6655418c231eb06dbbe606fbdf4
fatimaberazi <fatimaberaze1997@gmail.com>
2017-03-02 18:04:08 +0300

    Update simple_data_binding.js

--- js/simple_data_binding.js
+++ js/simple_data_binding.js
@@ -3,3 +3,13 @@ $(document).ready(function() {
     $('#myDiv').text($('textArea').val())
   })
 })
+//$(document).ready(function() {
+// typing(); 
+//}); 
+//function typing(){ 
+
+	//$('#textArea').on('keydown',function () {
+		//
+		//$('#myDiv').text($('#textArea').val()); 
+	//})
+//}
